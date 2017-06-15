a4dcc5c039dbc41bc9711ebeadc01f0ad1971417
橋本 浩良 <hashimotoh@toyo.co.jp>
2017-06-15 16:51:36 +0900

    tf_intro.py modified

--- tf_intro.py
+++ tf_intro.py
@@ -2,23 +2,24 @@
 import multiprocessing as mp
 import tensorflow as tf
 
-core_num = mp.cpu_count()
-config = tf.ConfigProto(
-    inter_op_parallelism_threads=core_num,
-    intra_op_parallelism_threads=core_num )
-sess = tf.Session(config=config)
+#core_num = mp.cpu_count()
+#config = tf.ConfigProto(
+#    inter_op_parallelism_threads=core_num,
+#    intra_op_parallelism_threads=core_num )
+#sess = tf.Session(config=config)
 
-hello = tf.constant('Hello, tensorflow')
+#hello = tf.constant('Hello, tensorflow')
 
-print(sess.run(hello))
+#print(sess.run(hello))
 
-a = tf.constant(1)
-b = tf.constant(32)
-print(sess.run(a+b))
+#a = tf.constant(1)
+#b = tf.constant(32)
+#print(sess.run(a+b))
 
 y = tf.placeholder(tf.float32, [1])
-x = tf.Variable(tf.zeros([1]))
-y = tf.pow(x,2) - 2*x + 10
+x1 = tf.Variable(tf.zeros([1]))
+x2 = tf.Variable(tf.zeros([1]))
+y = tf.pow(x1,2) - 2 * x1 + tf.pow(x2,2) + 4 * x2
 find_min = tf.train.GradientDescentOptimizer(0.5).minimize(y)
 
 sess = tf.InteractiveSession()
@@ -27,12 +28,3 @@ tf.global_variables_initializer().run()
 for _ in range(10):
     sess.run(find_min)
     print(sess.run(y))
-
-#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-#    
-#    sess = tf.InteractiveSession()
-#    tf.global_variables_initializer().run()
-#    # Train
-#    for _ in range(1000):
-#        batch_xs, batch_ys = mnist.train.next_batch(100)
-#        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
